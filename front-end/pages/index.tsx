e0edc3b5de9bd36094f687833a1a30cd646eeb73
Hayoung <gkduddy0814@gmail.com>
2021-09-11 17:29:46 +0900

    feat : 기본 폴더 세팅
    common / ui

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Container from '../components/ui/Container';
+
+const Main: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>instagram</title>
+        <meta name="description" content="instagram" />
+      </Head>
+
+      <Container>
+        <h1>메인페이지</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Main;
