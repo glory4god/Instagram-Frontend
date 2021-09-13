14f7b3d2d965344f7b1603fb4dcdcd7505dcfbe3
Hayoung <gkduddy0814@gmail.com>
2021-09-13 23:29:11 +0900

    feat : add LeftMenuBar in account pages
    메뉴 리스트만 추가하면 메뉴 늘어남

--- lib/redux/accounts/accountsApis.ts
+++ lib/redux/accounts/accountsApis.ts
@@ -1,11 +1,11 @@
 export function getAccountsIds() {
-  return Object.keys(accountNenuBar);
+  return Object.keys(accountMenuBar);
 }
 export function getAccountsValues() {
-  return Object.values(accountNenuBar);
+  return Object.values(accountMenuBar);
 }
 
-const accountNenuBar = {
+const accountMenuBar = {
   edit: '프로필 편집',
   password_change: '비밀번호 변경',
   manage_access: '앱 및 웹사이트',
