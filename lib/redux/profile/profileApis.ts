8d70bf6480ce1c5ed3722db0431165832d15bec2
Hayoung <gkduddy0814@gmail.com>
2021-09-13 16:48:53 +0900

    feat : profile 페이지
    테스트 이미지 and 데이터 추가

--- lib/redux/profile/profileApis.ts
+++ lib/redux/profile/profileApis.ts
@@ -1,4 +1,4 @@
-import { UserData } from 'types/profile/types';
+import type { UserData, BoardData } from 'types/profile/types';
 
 export async function getProfileData(pages: string) {
   const data: UserData[] = testData.filter((arr) => {
@@ -16,10 +16,20 @@ export async function getProfileIds() {
   return paths;
 }
 
+export async function getUserBoard(name: string) {
+  const filter: BoardData[] = board.filter((arr) => {
+    if (arr.name === name) {
+      return arr;
+    }
+  });
+  return filter[0];
+}
+
 const testData: UserData[] = [
   {
     id: 'winter',
-    board: 15,
+    name: '윈터',
+    board: 6,
     follower: 10272334,
     following: 100,
     introduce: '에스빠',
@@ -27,6 +37,7 @@ const testData: UserData[] = [
   },
   {
     id: 'irene',
+    name: '아이린',
     board: 3,
     follower: 10000000,
     following: 1002,
@@ -36,10 +47,38 @@ const testData: UserData[] = [
 
   {
     id: 'karina',
-    board: 3,
+    name: '카리나',
+    board: 4,
     follower: 1000090,
     following: 100,
     introduce: 'next level',
     imageUrl: '/profile/karina.png',
+  },
+];
+
+const board: BoardData[] = [
+  {
+    name: 'winter',
+    imageUrl: [
+      '/profile/winter1.png',
+      '/profile/winter2.png',
+      '/profile/winter3.png',
+      '/profile/winter4.png',
+      '/profile/winter5.png',
+      '/profile/winter6.png',
+    ],
+  },
+  {
+    name: 'karina',
+    imageUrl: [
+      '/profile/karina1.png',
+      '/profile/karina2.png',
+      '/profile/karina3.png',
+      '/profile/karina4.png',
+    ],
+  },
+  {
+    name: 'irene',
+    imageUrl: ['/profile/irene.png'],
   },
 ];
