dfdb9079ef57fcbbe91d9c4ab3750531c2c37f4e
Hayoung <gkduddy0814@gmail.com>
2021-09-13 16:52:10 +0900

    feat : add test type of boardData

--- types/profile/types.ts
+++ types/profile/types.ts
@@ -1,10 +1,16 @@
 export type UserData = {
   id: string;
+  name: string;
   board: number;
   follower: number;
   following: number;
   introduce: string;
   imageUrl: string;
+};
+
+export type BoardData = {
+  name: string;
+  imageUrl: string[];
 };
 
 export type Banner = 'main' | 'saved' | 'channel' | 'tagged';
